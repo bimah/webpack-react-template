18996d45129c27c5844b74deb3124c03e5cf143b
Matteo Corazzin <m.corazzin@syzygy.co.uk>
2016-11-21 09:22:55 +0000

    Removed HotModuleReplacementPlugin and add git repo on package

--- libs/webpack/DevServer.js
+++ libs/webpack/DevServer.js
@@ -1,5 +1,3 @@
-const webpack = require('webpack');
-
 exports.default = options =>
   ({
     watchOptions: {
@@ -32,11 +30,4 @@ exports.default = options =>
       host: options.host, // Defaults to `localhost`
       port: options.port, // Defaults to 8080
     },
-    plugins: [
-      // Enable multi-pass compilation for enhanced performance
-      // in larger projects. Good default.
-      new webpack.HotModuleReplacementPlugin({
-        multiStep: true,
-      }),
-    ],
   });
